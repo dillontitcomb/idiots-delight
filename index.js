6f4f05109740a43ac78ec998cb509fdc651069e5
Dillon Titcomb <dillontitcomb@gmail.com>
2019-07-10 17:08:35 -0700

    add generalized cardGroup class

--- index.js
+++ index.js
@@ -50,6 +50,32 @@ let game;
 // CLASSES
 
 //
+class CardGroup {
+	constructor(cards) {
+		this.cards = cards || [];
+		this.empty = this.cards.length ? false : true;
+		this.activeCard = this.empty ? null : this.cards[this.cards.length - 1];
+	}
+	shuffle() {
+		let currentIndex = this.cards.length,
+			temporaryValue,
+			randomIndex;
+		while (currentIndex !== 0) {
+			randomIndex = Math.floor(Math.random() * currentIndex);
+			currentIndex -= 1;
+			temporaryValue = this.cards[currentIndex];
+			this.cards[currentIndex] = this.cards[randomIndex];
+			this.cards[randomIndex] = temporaryValue;
+		}
+	}
+	transferCardsTo(recipient, numOfCards) {
+		for (let i = 0; i < numOfCards; i++) {
+			recipient.cards.push(this.cards.pop());
+		}
+	}
+}
+const group1 = new CardGroup([1, 2, 3, 4, 5, 6]);
+const group2 = new CardGroup();
 
 class Deck {
 	constructor() {
